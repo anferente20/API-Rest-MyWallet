58c29be34494bd99b00852492a3be3bfa923d899
Oscar Hernandez <ojhernandezcepeda@gmail.com>
2021-05-23 16:57:42 -0500

    no docker

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
 const app = express();
 
 app.use(morgan('dev'));
+app.use(cors())
 
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 //routes
 app.use(require('./routes/index'));
-
 
 module.exports = app;
