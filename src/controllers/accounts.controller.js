88154ba70d96b132148ed28288503cdbc50e51de
FelipeR97 <felipe@simian.co>
2021-05-25 19:55:38 -0500

    Edición cuentas

--- src/controllers/accounts.controller.js
+++ src/controllers/accounts.controller.js
@@ -12,23 +12,24 @@ const getAccounts = async (req, res) => {
     res.status(200).json(response.rows);
 };
 
+
 const createAccount = async (req, res) => {
     const {nombre,descripcion,idcliente} = req.body;
-    const response = await pool.query('INSERT INTO ACCOUNT (NOMBRE,DESCRIPCION,IDCLIENTE) VALUES ($1,$2,$3)',[nombre,descripcion,idcliente]);
+    const response = await pool.query('INSERT INTO ACCOUNTS (NOMBRE,DESCRIPCION,IDCLIENTE) VALUES ($1,$2,$3)',[nombre,descripcion,idcliente]);
     console.log(response);
     res.send('cuenta creada');
 }
 
 const editAccount = async (req, res)=>{
     const {nombre,descripcion, idaccount} = req.body;
-    const response = await pool.query('UPDATE ACCOUNT SET NOMBRE =$1 , DESCRIPCION=$2 WHERE ID = $3',[nombre,descripcion, idaccount]);
+    const response = await pool.query('UPDATE ACCOUNTS SET NOMBRE =$1 , DESCRIPCION=$2 WHERE ID = $3',[nombre,descripcion, idaccount]);
     console.log(response);
     res.send('cuenta actualizada');
 }
 
 const deleteAcount = async (req, res)=>{
     const {id} = req.body;
-    const response = await pool.query('DELETE FROM ACCOUNT WHERE ID = $1',[id]);
+    const response = await pool.query('DELETE FROM ACCOUNTS WHERE ID = $1',[id]);
     console.log(response);
     res.send('cuenta eliminada');
 }
